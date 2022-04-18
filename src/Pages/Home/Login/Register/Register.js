e599965a0d2e6e83f4858248bc6198c3087998f4
shtabdeepaull <shtabdee30@gmail.com>
2022-04-18 21:25:01 +0600

    added email verification code

--- src/Pages/Home/Login/Register/Register.js
+++ src/Pages/Home/Login/Register/Register.js
@@ -1,47 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { async } from '@firebase/util';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification:true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
     const navigate = useNavigate();
 
-    const navigateLogin = () =>{
+    const navigateLogin = () => {
         navigate('/login');
     }
 
-    if(user){
+    if (user) {
         navigate('/home');
     }
 
-    const handleRegister = event =>{
+    const handleRegister =async (event) => {
         event.preventDefault();
-        const name= event.target.name.value;
-        const email= event.target.email.value;
-        const password= event.target.password.value;
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        // const agree = event.target.terms.checked;
 
-        createUserWithEmailAndPassword(email, password);
+          await  createUserWithEmailAndPassword(email, password);
+          await updateProfile({ displayName:name });
+          navigate('/home')
+
+
     }
     return (
         <div className='register-form'>
-            <h2 style={{textAlign:'center'}}>Please Register</h2>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
             <form onSubmit={handleRegister}>
-                <input type="text" name="name" id=""  placeholder='Your Name'/>
-                <input type="email" name="email" id="" placeholder='Your Email' required/>
-                <input type="password" name="password" id="" placeholder='Your Password'required/>
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept Terms and Conditions </label>
-                <input className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="register" />
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : ' text-danger'}`} htmlFor="terms">Accept Terms and Conditions </label>
+                <input
+                    disabled={!agree}
+                    className='w-50 mx-auto btn btn-primary mt-2'
+                    type="submit"
+                    value="register" />
             </form>
             <p>Already have an Account?? <Link to="/login" className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             <SocialLogin></SocialLogin>
